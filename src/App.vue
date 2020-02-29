4ff1baa7a25e842c3a7439059e80813b56ab0938
leon <leonsk32@gmail.com>
2020-02-29 15:48:14 +0900

    add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
 <template>
-  <v-app>
+  <v-app style="padding-top: 50px">
+    <v-app-bar
+            color="deep-purple accent-4"
+            dense
+            dark
+            fixed
+    >
+      <v-toolbar-title>Anniversary Bingo</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu left>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item @click="goToBingo">
+            <v-list-item-title>Bingo</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="goToAdmin">
+            <v-list-item-title>Admin</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
     <router-view />
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import router from '@/router';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
-  data: () => ({
-    //
-  }),
+  methods: {
+    goToBingo: () => router.push('/'),
+    goToAdmin: () => router.push('/admin'),
+  },
 });
 </script>
